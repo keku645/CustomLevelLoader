66248d7575a9da4eaef76835890c59df0df9d347
keku645 <36018654+keku645@users.noreply.github.com>
2021-11-04 08:24:43 +0100

    Add Hit and Run

---
+++ ext/Shared/Levels/COOP_002/COOP_002_SquadRush0.lua
@@ -0,0 +1,117 @@
+return [[
+{
+	"data": [
+		{
+			"parentData": {
+				"partitionGuid": "00000000-0000-0000-0000-000000000000",
+				"guid": "00000000-0000-0000-0000-000000000000",
+				"primaryInstanceGuid": "00000000-0000-0000-0000-000000000000",
+				"typeName": "custom_root"
+			},
+			"guid": "83628134-c281-a75d-67b2-c90f1fdcf660",
+			"transform": {
+				"left": {
+					"x": 0,
+					"y": 0,
+					"z": -1
+				},
+				"up": {
+					"x": 0,
+					"y": 1,
+					"z": 0
+				},
+				"forward": {
+					"x": 1,
+					"y": 0,
+					"z": 0
+				},
+				"trans": {
+					"x": -16.043361663818,
+					"y": 0.81664103269577,
+					"z": 26.328216552734
+				}
+			},
+			"originalRef": {
+				"partitionGuid": "nil",
+				"instanceGuid": "nil"
+			},
+			"blueprintCtrRef": {
+				"name": "XP2/Objects/Skybar_Hoteldoor_02/SkybarHoteldoor_02",
+				"instanceGuid": "62FF6CAD-96BD-36B8-9D02-7594BC06C0E2",
+				"partitionGuid": "F527CEE2-50F2-11E1-BA95-E65D51ED76BA",
+				"typeName": "ObjectBlueprint"
+			},
+			"name": "XP2/Objects/Skybar_Hoteldoor_02/SkybarHoteldoor_02",
+			"variation": 0,
+			"origin": 2
+		},
+		{
+			"parentData": {
+				"partitionGuid": "00000000-0000-0000-0000-000000000000",
+				"guid": "00000000-0000-0000-0000-000000000000",
+				"primaryInstanceGuid": "00000000-0000-0000-0000-000000000000",
+				"typeName": "custom_root"
+			},
+			"guid": "25c65a64-bd21-fd25-ad63-ab88ebfc3590",
+			"transform": {
+				"left": {
+					"x": 0,
+					"y": 0,
+					"z": -1
+				},
+				"up": {
+					"x": 0,
+					"y": 1,
+					"z": 0
+				},
+				"forward": {
+					"x": 1,
+					"y": 0,
+					"z": 0
+				},
+				"trans": {
+					"x": -41.749126434326,
+					"y": -8.1812829971313,
+					"z": 15.440282821655
+				}
+			},
+			"originalRef": {
+				"partitionGuid": "nil",
+				"instanceGuid": "nil"
+			},
+			"blueprintCtrRef": {
+				"name": "XP2/Objects/Skybar_Hoteldoor_02/SkybarHoteldoor_02",
+				"instanceGuid": "62FF6CAD-96BD-36B8-9D02-7594BC06C0E2",
+				"partitionGuid": "F527CEE2-50F2-11E1-BA95-E65D51ED76BA",
+				"typeName": "ObjectBlueprint"
+			},
+			"name": "XP2/Objects/Skybar_Hoteldoor_02/SkybarHoteldoor_02",
+			"variation": 0,
+			"origin": 2
+		}
+	],
+	"header": {
+		"gameModeName": "Domination0",
+		"projectName": "SP_jet",
+		"timeStamp": 1636000062541,
+		"requiredBundles": {
+			"Levels/COOP_002/AB01_Art_Parent": true,
+			"gameconfigurations/game": true,
+			"Levels/COOP_002/General_Sluice_AB02_Parent": true,
+			"Levels/COOP_002/AB01_Parent": true,
+			"Levels/COOP_002/AB02_Parent": true,
+			"Levels/MP_Subway/MP_Subway_Settings_win32": true,
+			"Levels/COOP_002/AB03_Parent": true,
+			"Levels/COOP_002/AB02_Art_Parent": true,
+			"Levels/COOP_002/General_Sluice_AB03_Parent": true,
+			"Levels/XP2_Skybar/DeathMatch": true,
+			"Levels/COOP_002/COOP_002": true,
+			"Levels/XP2_Skybar/DOMINATION": true,
+			"Levels/COOP_002/AB04_Art_Parent": true,
+			"Levels/COOP_002/AB03_Art_Parent": true
+		},
+		"mapName": "COOP_002",
+		"id": 107
+	}
+}
+]]
